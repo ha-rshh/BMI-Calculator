7ef1a78a03f7abc29333f2fa1ce6d994c6ad703d
ha-rshh <prajapati.captain@gmail.com>
2025-02-17 01:05:25 +0530

    update 1

--- script.js
+++ script.js
@@ -8,20 +8,14 @@
 // Divide your weight by your height squared: 70 / 2.89 = 24.22
 
 const age = document.getElementById("age-input");
-const height = document.getElementById("height-input");
-const weight = document.getElementById("weight-input");
+const userHeight = document.getElementById("height-input");
+const userWeight = document.getElementById("weight-input");
 const calculateButton = document.getElementById("calc-btn");
 const resultContainer = document.getElementsByClassName("result-container");
 
 calculateButton.addEventListener("click", () => {
-  if (height.value == "" && weight.value == "") {
-    alert("bhai kuch likho to");
-  } else {
-    let bmi = Math.pow(height.value / weight.value, 2) * 10000;
-    console.log(Math.round(bmi));
-
-    let result = document.createElement("div");
-    result.innerText = bmi;
-    // resultContainer.innerText(result)
-  }
+  let height = userHeight.value;
+  let weight = userWeight.value;
+  let bmiValue = weight / ((height * height) / 10000).toFixed(2);
+  resultContainer.innerText = bmiValue;
 });
